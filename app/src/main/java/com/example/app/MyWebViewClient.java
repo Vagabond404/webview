b5436e25bb9b06a2d7ab49ded33f63539fb9fc2e
Ange LABBE <150508579+Vagabond404@users.noreply.github.com>
2026-03-19 12:45:31 +0700

    Refactor MyWebViewClient to check specific hostname

--- app/src/main/java/com/example/app/MyWebViewClient.java
+++ app/src/main/java/com/example/app/MyWebViewClient.java
@@ -5,19 +5,14 @@ import android.net.Uri;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-class MyWebViewClient extends WebViewClient {
+public class MyWebViewClient extends WebViewClient {
 
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        String hostname;
-
-        // YOUR HOSTNAME
-        hostname = "example.com";
-
-        Uri uri = Uri.parse(url);
-        if (url.startsWith("file:") || uri.getHost() != null && uri.getHost().endsWith(hostname)) {
+        if(Uri.parse(url).getHost().endsWith("roneat-studio-pro.gitbook.io")) {
             return false;
         }
+
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         view.getContext().startActivity(intent);
         return true;
